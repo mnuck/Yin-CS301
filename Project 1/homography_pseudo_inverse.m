6929c7ca78cdb6467ee4beeb823da1ce9254a290
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2012-02-07 01:19:03 -0600

    both homography matrix calculators complete

--- Project 1/homography_pseudo_inverse.m
+++ Project 1/homography_pseudo_inverse.m
@@ -11,8 +11,6 @@ function [ result ] = homography_pseudo_inverse( source_x, source_y, dest_x, des
     A = [ source_x source_y vec1 vec0     vec0     vec0 first  third ; ...
           vec0     vec0     vec0 source_x source_y vec1 second fourth ];
     b = [ dest_x ; dest_y ];
-    
     h = A \ b;
-    
-    result = [ h(1) h(2) h(3) ; h(4) h(5) h(6) ; h(7) h(8) 1 ];
+    result = transpose([h(1:3) h(4:6) [h(7:8) ; 1]]);
 end
--- Project 1/homography_svd.m
+++ Project 1/homography_svd.m
@@ -1,7 +1,17 @@
-function [ h ] = homography_svd( source_x, source_y, dest_x, dest_y )
-%HOMOGRAPHY_SVD Summary of this function goes here
-%   Detailed explanation goes here
-
-h = [];
+function [ result ] = homography_svd( source_x, source_y, dest_x, dest_y )
+%HOMOGRAPHY_SVD Compute the homography matrix via singular value decomp
+    vec1 = ones( length(source_x), 1 );
+    vec0 = zeros( length(source_x), 1 );
+    
+    first   = -1 * source_x .* dest_x; % column vectors
+    second  = -1 * source_x .* dest_y; % built up here
+    third   = -1 * source_y .* dest_x; % for clarity
+    fourth  = -1 * source_y .* dest_y;
+    fifth   = -1 * dest_x;
+    sixth   = -1 * dest_y;
+    
+    A = [ source_x source_y vec1 vec0     vec0     vec0 first  third  fifth ; ...
+          vec0     vec0     vec0 source_x source_y vec1 second fourth sixth ];
+    [~, ~, V] = svd(transpose(A) * A);
+    result = transpose( [V(1:3,end) V(4:6,end) V(7:9,end)] );           
 end
-
