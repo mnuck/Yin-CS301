d0d665a9861c7414ec014f78a2a3d8e4454da03c
Michael Wisely <michaelwisely@gmail.com>
2012-02-26 13:51:47 -0600

    Updated the Makefile to build on ubuntu, updated .gitignore to ignore the executable, and updated main.cpp to use the C++ opencv stuff

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -1,60 +1,48 @@
-////////////////////////////////////////////////////////////////////////
-//
-// hello-world.cpp
-//
-// This is a simple, introductory OpenCV program. The program reads an
-// image from a file, inverts it, and displays the result. 
-//
-////////////////////////////////////////////////////////////////////////
 #include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <cv.h>
-#include <highgui.h>
+#include <iostream>
+#include "cv.h"
+#include "highgui.h"
 
+using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
-  IplImage* img = 0; 
-  int height,width,step,channels;
-  uchar *data;
-  int i,j,k;
-
-  if(argc<2){
-    printf("Usage: main <image-file-name>\n\7");
+  if(argc != 3) {
+    cout << "Usage: " << argv[0] << " <left image> <right image>" << endl;
     exit(0);
   }
 
-  // load an image  
-  img=cvLoadImage(argv[1]);
-  if(!img){
-    printf("Could not load image file: %s\n",argv[1]);
+  // load the images
+  Mat left_image  = imread(argv[1]);
+  Mat right_image = imread(argv[2]);
+
+  // Make sure the files opened
+  if(!left_image.data || !right_image.data) {
+    cout << "Could not load one of the image files." << endl;
     exit(0);
   }
-
-  // get the image data
-  height    = img->height;
-  width     = img->width;
-  step      = img->widthStep;
-  channels  = img->nChannels;
-  data      = (uchar *)img->imageData;
-  printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
 
   // create a window
-  cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
-  cvMoveWindow("mainWin", 100, 100);
+  namedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
 
-  // invert the image
-  for(i=0;i<height;i++) for(j=0;j<width;j++) for(k=0;k<channels;k++)
-                                               data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
+  int height    = left_image.rows;
+  int width     = left_image.cols;
+  int channels  = left_image.channels();
+
+  cout << "Height: " << height << endl
+       << "Width: " << width << endl
+       << "Channels: " << channels << endl;
+
+  Mat inverter(left_image.size(), left_image.type(), Scalar(255, 255, 255));
+
+  left_image = inverter - left_image;
 
   // show the image
-  cvShowImage("mainWin", img );
+  imshow("mainWin", left_image);
 
   // wait for a key
-  cvWaitKey(0);
+  waitKey(0);
 
-  // release the image
-  cvReleaseImage(&img );
   return 0;
 }
