e43d66b22247f49c98831a55f5eb42d22fa9dc25
Michael Wisely <michaelwisely@gmail.com>
2012-03-01 15:07:54 -0600

    Looks pretty decent.

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -32,7 +32,7 @@ void intensity_normalize(const Mat& matrix, Mat& normalized) {
 }
 
 void compute_cost_matrix(const Mat& dsi, Mat& costs, Mat& paths, 
-                         const float occlusion_cost=100){
+                         const float occlusion_cost=50){
   costs = Mat(dsi.size(), CV_32FC1, Scalar(INFINITY));
   paths = Mat(dsi.size(), CV_8UC1, Scalar(NAN));
 
@@ -85,10 +85,7 @@ void compute_cost_matrix(const Mat& dsi, Mat& costs, Mat& paths,
   }
 }
 
-double NCC(const Mat& f, const Mat& g){
-  Mat f_hat, g_hat;
-  intensity_normalize(f, f_hat);
-  intensity_normalize(g, g_hat);
+double NCC(const Mat& f_hat, const Mat& g_hat){
   return 1-f_hat.dot(g_hat);
 }
 
@@ -96,26 +93,26 @@ void DSI_method(const Mat& left, const Mat& right,
                 Mat& dest, const int half_patch_height=4){
   int patch_height = half_patch_height*2+1;
 
-  dest = Mat(left.rows, right.rows, CV_8U, Scalar(255));
+  dest = Mat(left.size(), CV_8U, Scalar(0));
 
   namedWindow("pathWin", CV_WINDOW_AUTOSIZE);
   namedWindow("costWin", CV_WINDOW_AUTOSIZE);
   namedWindow("accWin", CV_WINDOW_AUTOSIZE);
 
   for(int i = 0; i < left.rows-patch_height; i++) {
-    Mat l_row, r_row; 
+    Mat l_row, r_row, l_patch, r_patch, l_patch_norm, r_patch_norm; 
     Mat dsi(left.cols, right.cols, CV_32FC1, Scalar(INFINITY));
 
-    // intensity_normalize(left.rowRange(i, i+patch_height), l_row);
-    // intensity_normalize(right.rowRange(i, i+patch_height), r_row);
     l_row = left.rowRange(i, i+patch_height);
     r_row = right.rowRange(i, i+patch_height);
 
     for(int j = 0; j < left.cols-patch_height; j++) {
-      Mat l_patch = l_row.colRange(j, j + patch_height);
+      l_patch = l_row.colRange(j, j + patch_height);
+      intensity_normalize(l_patch, l_patch_norm);
       for(int k = (j >= TOLERANCE? j-TOLERANCE : 0); k < j; k++) {
-        Mat r_patch = r_row.colRange(k, k + patch_height);
-        dsi.at<float>(k, j) = NCC(l_patch, r_patch);
+        r_patch = r_row.colRange(k, k + patch_height);
+        intensity_normalize(r_patch, r_patch_norm);
+        dsi.at<float>(k, j) = NCC(l_patch_norm, r_patch_norm);
       }
     }
 
@@ -162,8 +159,6 @@ void DSI_method(const Mat& left, const Mat& right,
       Point pt = Point(x, y);
       unsigned char val = paths.at<unsigned char>(pt);
 
-      dest.at<unsigned char>(i, x) = (unsigned char)abs((y - x)*255/64);
-
       // Add path to image
       rectangle(temp, pt, pt, Scalar(0, 255, 255));
 
@@ -172,6 +167,7 @@ void DSI_method(const Mat& left, const Mat& right,
         done = true;
         break;
       case MATCH:
+        dest.at<unsigned char>(i, x) = (unsigned char)abs((y - x)*255/64);
         x--; y--;
         break;
       case OCCLUDED_FROM_LEFT:
@@ -197,42 +193,6 @@ void DSI_method(const Mat& left, const Mat& right,
   }  
 }
 
-Mat match_epipolar_lines(const Mat& f, const Mat& g, const int half_patch_height=4){
-  Mat result(f.size(), CV_8U, Scalar(0));
-  Mat temp;
-
-  int patch_height = half_patch_height*2+1;
-  double largest_score;
-  int largest_index;
-
-  namedWindow("matchresult", CV_WINDOW_AUTOSIZE);
-
-  for(int i = 0; i < f.rows-patch_height; i++) {
-    cout << "Computing row: " << i << endl;
-    for(int j = 0; j < f.cols-patch_height; j++) {
-      Mat f_patch = f(Range(i, i + patch_height), Range(j, j + patch_height));
-      bool run_once = false;
-      for(int k = (j >= TOLERANCE? j-TOLERANCE : 0); 
-          k < j+TOLERANCE && k < (g.cols-patch_height); k++) {
-        Mat g_patch = g(Range(i, i + patch_height), Range(k, k + patch_height));
-        double score = NCC(f_patch, g_patch);
-        if(score > largest_score || !run_once){
-          run_once = true;
-          largest_score = score;
-          largest_index = k;
-        }
-      }
-      result.at<unsigned char>(i, j) = (unsigned char)abs(largest_index - j)*3;
-    }
-
-    result.convertTo(temp, CV_8U);
-    imshow("matchresult", temp);
-    waitKey(1);
-  }
-  return result;
-}
-
-
 int main(int argc, char *argv[])
 {
   if(argc != 3) {
@@ -254,9 +214,9 @@ int main(int argc, char *argv[])
   cvtColor(left_image_color, left_image_gray, CV_BGR2GRAY);
   cvtColor(right_image_color, right_image_gray, CV_BGR2GRAY);
 
-  Mat left_image, right_image;
-  GaussianBlur(left_image_gray, left_image, Size(3, 3), 30, 30);
-  GaussianBlur(right_image_gray, right_image, Size(3, 3), 30, 30);
+  Mat left_image=left_image_gray, right_image=right_image_gray;
+  // GaussianBlur(left_image_gray, left_image, Size(3, 3), 30, 30);
+  // GaussianBlur(right_image_gray, right_image, Size(3, 3), 30, 30);
 
   // Make them 32 bit signed...
   left_image.convertTo(left_image, CV_32SC1);
@@ -273,7 +233,7 @@ int main(int argc, char *argv[])
   // Mat match = match_epipolar_lines(left_image, right_image, 5);
 
   Mat dsi;
-  DSI_method(left_image, right_image, dsi, 5);
+  DSI_method(left_image, right_image, dsi, 3);
 
   // show the image
   namedWindow("dsiWin", CV_WINDOW_AUTOSIZE);
